292b4e90cb9d50c328b8c7c2545689ecf5a51df7
Philipp Wruck <philipp@perspective.co>
2017-10-24 16:49:22 +0200

    Adds webpack-bundle-analyzer

--- src/next.config.js
+++ src/next.config.js
@@ -1,8 +1,24 @@
+const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
+
+const { ANALYZE } = process.env;
+
 module.exports = {
-    exportPathMap : function() {
+    webpack : ( config ) => {
+        if ( ANALYZE ) {
+            config.plugins.push(
+                new BundleAnalyzerPlugin( {
+                    analyzerMode    : 'static',
+                    reportFilename  : './analyze-report.html',
+                } ),
+            );
+        }
+
+        return config;
+    },
+    exportPathMap : () => {
         return {
-            '/'               : { page : '/' },
-            '/about'          : { page : '/about' },
+            '/'      : { page : '/' },
+            '/about' : { page : '/about' },
         };
     },
 };
