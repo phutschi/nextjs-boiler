298cd641b39bae9d468ad59c85229fdb66a12a1a
Philipp Wruck <philipp@perspective.co>
2017-10-24 18:03:45 +0200

    Adds lru-caching

--- src/next.config.js
+++ src/next.config.js
@@ -1,10 +1,10 @@
-const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
-
 const { ANALYZE } = process.env;
 
 module.exports = {
     webpack : ( config ) => {
         if ( ANALYZE ) {
+            const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
+
             config.plugins.push(
                 new BundleAnalyzerPlugin( {
                     analyzerMode    : 'static',
