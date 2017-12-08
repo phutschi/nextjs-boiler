e48a8410651f357e4dc79e28bde7433d187d6ef1
Philipp Wruck <philipp@perspective.co>
2017-12-08 12:34:56 +0100

    adds routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+// @flow
+
+type Route = {
+    name: string,
+    path: string
+}
+
+const routes: Array<Route> = [
+    {
+        name : 'index',
+        path : '/',
+    },
+];
+
+export default routes
