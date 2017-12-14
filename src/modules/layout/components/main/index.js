a74e32e0a4b63c80240cc0f0a0cf4275770d8b2e
Philipp Wruck <philipp@perspective.co>
2017-12-14 11:41:27 +0100

    adds main component

---
+++ src/modules/layout/components/main/index.js
@@ -0,0 +1,32 @@
+// @flow
+
+import * as React from 'react'
+
+type Props = {
+    black?   : boolean,
+    children : React.Node,
+}
+
+const Main = ( { black, children } : Props ) => (
+    <div className={`main ${black ? 'black' : ''}`}>
+        {children}
+        <style jsx>
+            {`
+                .main {
+                    background-color: white;
+                    color: black;
+                    margin: 0;
+                }
+
+                .black {
+                    background-color: black;
+                    color: white;
+                }
+            `}
+        </style>
+    </div>
+)
+
+Main.defaultProps = { black : false }
+
+export default Main
