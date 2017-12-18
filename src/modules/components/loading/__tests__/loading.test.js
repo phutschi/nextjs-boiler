a0fc9967f13dbf722d2538ee1e810b194dafd70e
Philipp Wruck <philipp@perspective.co>
2017-12-18 10:18:46 +0100

    renames all tests

---
+++ src/modules/components/loading/__tests__/loading.test.js
@@ -0,0 +1,26 @@
+/* global React, expect */
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Button from 'components/button';
+
+configure( { adapter : new Adapter() } );
+
+describe( 'Button Component', () => {
+    it( 'should render', () => {
+        const wrapper = shallow( <Button /> );
+
+        expect( wrapper.exists ).toBeTruthy();
+    } );
+    it( 'should set defaultProps', () => {
+        const wrapper = shallow( <Button /> );
+
+        expect( wrapper.text() ).toEqual( '' );
+    } );
+    it( 'should have text prop as text', () => {
+        const wrapper = shallow( <Button text="Submit" /> );
+
+        expect( wrapper.text() ).toEqual( 'Submit' );
+    } );
+} );
